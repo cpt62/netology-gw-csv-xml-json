842f49431d425e8e444db6ecbf8acf71685e7bd5
cpt62 <rgrturzn7316@gmail.com>
2026-07-07 19:13:54 +0300

    EmployeeJsonBuilder.class created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
+    public static void main(String[] args) {
+        CsvToBean<Employee> toBean;
+
+        ColumnPositionMappingStrategy<Employee> strategy
+                = new ColumnPositionMappingStrategy<>();
+        strategy.setType(Employee.class);
+        strategy.setColumnMapping("id", "firstName", "lastName", "country", "age");
+
+
+        try (FileReader fr = new FileReader("data.csv")) {
+            toBean =
+                    new CsvToBeanBuilder<Employee>(fr)
+                            .withSeparator(',')
+                            .withMappingStrategy(strategy)
+                            .build();
+
+            EmployeeJsonBuilder employeeJsonBuilder = new EmployeeJsonBuilder();
+            employeeJsonBuilder.create("data.json", toBean);
+
+        } catch (IOException ioException) {
+            System.out.println(ioException.getMessage());
+        }
+
+    }
 }
